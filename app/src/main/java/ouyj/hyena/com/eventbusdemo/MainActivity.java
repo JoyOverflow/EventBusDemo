83f2226ed18254e7b65af549bb2d8829a2869bb5
joyoverflow <joyoverflow@sina.com>
2019-06-29 16:35:23 +0800

    添加演示

--- app/src/main/java/ouyj/hyena/com/eventbusdemo/MainActivity.java
+++ app/src/main/java/ouyj/hyena/com/eventbusdemo/MainActivity.java
@@ -8,13 +8,29 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
+/**
+ * EventBus（Event事件，Subscriber订阅者，Publisher发布者）事件发布/订阅框架
+ * 1.首先在一个活动里注册EventBus事件和解除注册
+ * 2.定义订阅者并指定线程模型
+ *   Posting (默认) 表示发布者和事件处理在同一个线程
+ *   Main 表示事件处理在主(UI)线程（因此不能进行耗时操作）
+ *   Background 如果发布者在主(UI)线程那么事件处理会开启一个后台线程，如果发布者是在后台线程那么事件处理也会使用该线程。
+ *   Async 事件处理始终新建一个线程来运行（不能进行UI操作）
+ * 3.自定义事件类（用来传输）
+ * 4.定义发送者来发送自定义事件类
+ */
 public class MainActivity extends AppCompatActivity {
 
     private Button mButton;
     private TextView mText;
 
-
+    /**
+     * 注册EventBus
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,13 +48,11 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
         EventBus.getDefault().register(this);
-
     }
-
-
+    /**
+     * 解除EventBus
+     */
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -46,5 +60,14 @@ public class MainActivity extends AppCompatActivity {
         if(EventBus.getDefault().isRegistered(this)) {
             EventBus.getDefault().unregister(this);
         }
+    }
+    /**
+     * 声明订阅者来处理事件（必需加上特定注解）
+     * 线程模式为运行在主（UI）线程，因此不能进行耗时操作
+     * @param messageEvent
+     */
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void receiveEvent(Message messageEvent) {
+        mText.setText(messageEvent.getTitle());
     }
 }
--- app/src/main/java/ouyj/hyena/com/eventbusdemo/ResultActivity.java
+++ app/src/main/java/ouyj/hyena/com/eventbusdemo/ResultActivity.java
@@ -1,7 +1,9 @@
 package ouyj.hyena.com.eventbusdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.greenrobot.eventbus.EventBus;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        //发送非粘性事件并关闭此活动（事件发布者Publisher）
+        EventBus.getDefault().post(new Message("欢迎大家访问我的博客!"));
+        finish();
     }
 }
+
