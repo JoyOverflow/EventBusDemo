631c98f3743c1339e7b7a240ab3b704eee7b9a87
joyoverflow <joyoverflow@sina.com>
2019-06-29 12:47:31 +0800

    添加EventBus

--- app/src/main/java/ouyj/hyena/com/eventbusdemo/MainActivity.java
+++ app/src/main/java/ouyj/hyena/com/eventbusdemo/MainActivity.java
@@ -1,13 +1,50 @@
 package ouyj.hyena.com.eventbusdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.greenrobot.eventbus.EventBus;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private TextView mText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mText = findViewById(R.id.txtTile);
+        mButton = findViewById(R.id.btnTest);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(
+                        MainActivity.this,
+                        ResultActivity.class
+                );
+                startActivity(intent);
+            }
+        });
+
+
+        EventBus.getDefault().register(this);
+
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if(EventBus.getDefault().isRegistered(this)) {
+            EventBus.getDefault().unregister(this);
+        }
     }
 }
---
+++ app/src/main/java/ouyj/hyena/com/eventbusdemo/Message.java
@@ -0,0 +1,15 @@
+package ouyj.hyena.com.eventbusdemo;
+
+
+public class Message {
+    private String title;
+    public  Message(String title){
+        this.title=title;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
